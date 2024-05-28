d8a6087adf72f5121097ce1a6f3cb793fe0962a7
sage-etcher <sage.codes@email.com>
2024-05-28 10:53:30 -0400

    added flags to link using mingw on windows

--- src/nonportable_windows.c
+++ src/nonportable_windows.c
@@ -43,47 +43,6 @@ close_gui_term (void)
 }
 
 
-char *
-open_file_prompt (void)
-{
-    /*{{{*/
-    OPENFILENAME ofn;
-    size_t file_len = 260;
-    char *file_buf;
-
-    file_buf = SDL_malloc (file_len * sizeof (char));
-    if (!file_buf)
-        return NULL;
-
-    /* initialize OPENFILENAME */
-    ZeroMemory (&ofn, sizeof (ofn));
-    ofn.lStructSize = sizeof (ofn);
-    ofn.hwndOwner = GetActiveWindow ();
-    ofn.lpstrFile = file_buf;
-    ofn.lpstrFile[0] = '\0';
-    ofn.nMaxFile = file_len;
-    ofn.lpstrFilter = "All\0"   "*.*\0"
-                      "Image\0" "*.PNG;*.JPG;*.JPEG\0"
-                      "PNG\0"   "*.PNG\0"
-                      "JPEG\0"  "*.JPG;*.JPEG\0";
-    ofn.nFilterIndex = 2;
-    ofn.lpstrFileTitle = NULL;
-    ofn.nMaxFileTitle= 0;
-    ofn.lpstrInitialDir = NULL;
-    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
-
-    /* display the open dialog box. */
-    if (GetOpenFileName (&ofn) == TRUE)
-    {
-        return ofn.lpstrFile;
-    }    
-
-    SDL_free (file_buf);
-    return NULL;
-    /*}}}*/
-}
-
-
 #endif /* _WIN32 */
 
 /*
